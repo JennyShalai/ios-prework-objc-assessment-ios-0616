3a310e25868542b42c4b4228375ffda8603d12e3
JennyShalai <jenny.shalai@gmail.com>
2016-06-02 10:17:14 -0400

    done

--- ios-prework-objc-assessment/AppDelegate.m
+++ ios-prework-objc-assessment/AppDelegate.m
@@ -15,8 +15,51 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    
+    NSMutableArray *numbersArray = [[NSMutableArray alloc] init];
+    numbersArray = [[self generateArrayOfNumbers] mutableCopy];
+    
+    NSMutableArray *lettersArray = [[NSMutableArray alloc] init];
+    lettersArray = [[self generateArrayOfLetters] mutableCopy];
+    
+    NSMutableDictionary *alphabetDictionary = [[NSMutableDictionary alloc] init];
+    alphabetDictionary = [[self generateNumbersAndLettersDictionaryWithNumbers:numbersArray letters:lettersArray] mutableCopy];
+    
+    NSLog(@"%@", numbersArray);
+    NSLog(@"%@", lettersArray);
+    NSLog(@"%@", alphabetDictionary);
     
     return YES;
 }
+
+- (NSArray *)generateArrayOfNumbers {
+    NSMutableArray *numbersArray = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 1; i < 27; i++) {
+        [numbersArray addObject:@(i)];
+    }
+    return numbersArray;
+}
+
+- (NSArray *)generateArrayOfLetters{
+    NSMutableArray *lettersArray = [[NSMutableArray alloc] init];
+    char letter = 'a';
+    for (NSUInteger i = 0; i < 26; i++) {
+        char currentLetter = letter + i;
+        NSString *stringLetter = [NSString stringWithFormat:@"%c", currentLetter];
+        [lettersArray addObject:stringLetter];
+    }
+    return lettersArray;
+}
+
+- (NSDictionary *)generateNumbersAndLettersDictionaryWithNumbers:(NSArray *)numbersArray letters:(NSArray *)lettersArray {
+    NSMutableDictionary *numbersAndLettersDictionary = [[NSMutableDictionary alloc] init];
+    for (NSUInteger i = 0; i < 26; i++) {
+        numbersAndLettersDictionary[numbersArray[i]] = lettersArray[i];
+    }
+    return numbersAndLettersDictionary;
+}
+
+
 
 @end
